5cf62436d38d6b987bddb4df695fe0ec865d17fb
RitaClose <104946160+RitaClose@users.noreply.github.com>
2023-09-19 11:15:15 +1200

    Dabbling

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_4.jpg";
-let maskFile   = "mask_4.png";
-let outputFile = "output_4.png";
+let sourceFile = "input_1.jpg";
+let maskFile   = "mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -18,25 +18,37 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255, 0, 0);
+  // background(255, 0, 0);
+  background(sourceFile);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 function draw () {
-  for(let i=0;i<4000;i++) {
+  for(let i = 0; i < 5000; i ++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
-    fill(pix);
-    if(mask[0] > 128) {
+    fill(pix[0], 0, 0);
+    noStroke();
+    if(mask[0] > 128) { // white part of mask
       let pointSize = 10;
-      ellipse(x, y, pointSize, pointSize);
+      // stroke(pix[0], 0, 0);
+      stroke(pix);
+      strokeWeight(pointSize / 5);
+      line(x - pointSize, y - pointSize, x + pointSize, y + pointSize);   
+      line(x - pointSize, y + pointSize, x + pointSize, y - pointSize);
     }
-    else {
-      let pointSize = 20;
-      rect(x, y, pointSize, pointSize);    
+    else { // black part of mask
+      let pointSize = 15;
+      fill(pix);
+      stroke(pix[0], 20, 255, 100);
+      strokeWeight(pointSize / 5);   
+      line(x, y - pointSize - 10, x, y + pointSize + 10); 
+      stroke(pix);
+      line(x, y - pointSize, x, y + pointSize); 
+      // line(x - pointSize, y, x + pointSize, y);
     }
   }
   renderCounter = renderCounter + 1;
